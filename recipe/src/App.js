14035c047e6a7ec4d697ba34dbeca20836528ccd
Rahul_Thakur30 <rahulthakurnita@gmail.com>
2017-12-26 15:57:29 +0530

    basic structure of form

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -8,15 +8,80 @@ class App extends Component {
   render() {
     return (
     <div>
-    
+      <div className="container">
+      <nav class="navbar navbar-light">
+        <a class="navbar-brand" href="#">Recipe Planner</a>
+      </nav>
+      </div>
+
+
       <form>
        
        
-        <div className="form-group">
-          <label >Recipe Name</label>
-          <input type ="text" className="form-control" className ="col-sm-2"/>
+        <div className="row">
+          <div className="col">
+            <input type ="text" className="form-control" placeholder="Recipe Name"/>
+          </div>
+          
+          <div className="col">
+            <input type="text" className="form-control" placeholder="Maximum Cooking Time in Minutes"/>
+          </div>
         </div>
-     
+
+        <div className="row">
+          <div className="col">
+            <input type ="text" className="form-control" placeholder="Ingredients you have"/>
+          </div>
+
+          <div className="col">
+            <input type ="text" className="form-control" placeholder="Ingredients you don't have"/>
+          </div>
+        </div>
+
+
+        <div className="row">
+          <div className="col">
+            <input type ="text" className="form-control" placeholder="Allergies"/>
+          </div>
+
+          <div className="col">
+            <input type ="text" className="form-control" placeholder="Specific diets"/>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <input type ="text" className="form-control" placeholder="Cuisines you like"/>
+          </div>
+
+          <div className="col">
+            <input type ="text" className="form-control" placeholder="Cuisines you don't like"/>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <input type ="text" className="form-control" placeholder="Courses you want"/>
+          </div>
+
+          <div className="col">
+            <input type ="text" className="form-control" placeholder="Courses you don't want"/>
+          </div>
+        </div>
+
+
+
+        <div className="row">
+          <div className="col">
+            <input type ="text" className="form-control" placeholder="Holidays/Season you want"/>
+          </div>
+
+          <div className="col">
+            <input type ="text" className="form-control" placeholder="Holidays/Season you don't want"/>
+          </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
      
      
      
