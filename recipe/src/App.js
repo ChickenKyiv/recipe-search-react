245fa88959d13b654a1978299d5a015164dcb1b7
Rahul_Thakur30 <rahulthakurnita@gmail.com>
2017-12-26 14:10:23 +0530

    add starting label

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
+const formFunction =()=>{
+
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        react App
+    <div>
+    
+      <form>
+       
+       
+        <div className="form-group">
+          <label >Recipe Name</label>
+          <input type ="text" className="form-control" className ="col-sm-2"/>
+        </div>
+     
+     
+     
+     
+      </form>
+      
+
       </div>
     );
   }
