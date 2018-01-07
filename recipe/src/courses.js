a6f3df5ebc88857af02fc513d5394299a8567100
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-01-07 22:33:18 +0300

    +++

--- recipe/src/courses.js
+++ recipe/src/courses.js
@@ -7,108 +7,56 @@ function getCourses (){
 
      var courses     = [
      {
-          // "id":"course-Main Dishes",
           "name":"Main Dishes",
-          "type":"course",
-          // "description":"Main Dishes",
-          // "searchValue":"course^course-Main Dishes",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Desserts",
           "name":"Desserts",
-          "type":"course",
-          // "description":"Desserts",
-          // "searchValue":"course^course-Desserts",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Side Dishes",
           "name":"Side Dishes",
-          "type":"course",
-          // "description":"Side Dishes",
-          // "searchValue":"course^course-Side Dishes",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Appetizers",
           "name":"Appetizers",
-          "type":"course",
-          // "description":"Appetizers",
-          // "searchValue":"course^course-Appetizers",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Salads",
           "name":"Salads",
-          "type":"course",
-          // "description":"Salads",
-          // "searchValue":"course^course-Salads",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Breakfast and Brunch",
           "name":"Breakfast and Brunch",
-          "type":"course",
-          // "description":"Breakfast and Brunch",
-          // "searchValue":"course^course-Breakfast and Brunch",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Breads",
           "name":"Breads",
-          "type":"course",
-          // "description":"Breads",
-          // "searchValue":"course^course-Breads",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Soups",
           "name":"Soups",
-          "type":"course",
-          // "description":"Soups",
-          // "searchValue":"course^course-Soups",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Beverages",
           "name":"Beverages",
-          "type":"course",
-          // "description":"Beverages",
-          // "searchValue":"course^course-Beverages",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Condiments and Sauces",
           "name":"Condiments and Sauces",
-          "type":"course",
-          // "description":"Condiments and Sauces",
-          // "searchValue":"course^course-Condiments and Sauces",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Cocktails",
           "name":"Cocktails",
-          "type":"course",
-          // "description":"Cocktails",
-          // "searchValue":"course^course-Cocktails",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Snacks",
           "name":"Snacks",
-          "type":"course",
-          // "description":"Snacks",
-          // "searchValue":"course^course-Snacks",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      },
      {
-          // "id":"course-Lunch",
           "name":"Lunch",
-          "type":"course",
-          // "description":"Lunch",
-          // "searchValue":"course^course-Lunch",
-          // "localesAvailableIn":["en-US"]
+          "type":"course"
      }
      ];
 
