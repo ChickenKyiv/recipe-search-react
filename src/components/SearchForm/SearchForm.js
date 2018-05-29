449ca36c2853bddd0887256be32efc6f655f58bd
Arthur Tkachenko <atherdon@gmail.com>
2018-05-29 04:39:29 +0300

    xxx

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {
   Form,
-  Input, 
+  Input,
   Col,
   Button
 } from 'antd';
@@ -18,7 +18,7 @@ class SearchForm extends Component {
 
   constructor(props) {
     super(props)
-    
+
     this.state = {
       formLayout: 'vertical',
       maxTime: '',
@@ -102,13 +102,18 @@ class SearchForm extends Component {
               <Input placeholder="Recipe Name" />
             </Col>
             <Col span="12">
-              <Input id="time" onChange={this.updateMaxTime.bind(this)} placeholder="Maximum Cooking Time in Minutes" />
+              <Input id="time" onChange={this.updateMaxTime.bind(this)}
+                placeholder="Maximum Cooking Time in Minutes" />
             </Col>
           </InputGroup>
           <br />
           <InputGroup size="large" {...formItemLayout}>
-            <Ingredient updateIng={this.updateHaveIngredients.bind(this)} sign="1" passedSelected={this.state.dontHaveIngredients} placeholder="Ingredients you have" />
-            <Ingredient updateIng={this.updateDontHaveIngredients.bind(this)} sign="0" passedSelected={this.state.haveIngredients} placeholder="Ingredients you don't have" />
+            <Ingredient updateIng={this.updateHaveIngredients.bind(this)}
+              sign={true} passedSelected={this.state.dontHaveIngredients}
+              placeholder="Ingredients you have" />
+            <Ingredient updateIng={this.updateDontHaveIngredients.bind(this)}
+              sign={false} passedSelected={this.state.haveIngredients}
+              placeholder="Ingredients you don't have" />
 
           </InputGroup>
           <br />
@@ -124,21 +129,33 @@ class SearchForm extends Component {
           </InputGroup>
           <br />
           <InputGroup size="large" {...formItemLayout}>
-            <Cuisine updateCuisines={this.updateLikeCuisines.bind(this)} passedSelected={this.state.dontLikeCuisines} sign="1" placeholder="Cuisines you like" />
-            <Cuisine updateCuisines={this.updateDontLikeCuisines.bind(this)} passedSelected={this.state.likeCuisines} sign="0" placeholder="Cuisines you don't like" />
+            <Cuisine updateCuisines={this.updateLikeCuisines.bind(this)}
+              passedSelected={this.state.dontLikeCuisines} sign={true}
+              placeholder="Cuisines you like" />
+            <Cuisine updateCuisines={this.updateDontLikeCuisines.bind(this)}
+              passedSelected={this.state.likeCuisines} sign={false}
+              placeholder="Cuisines you don't like" />
           </InputGroup>
           <br />
           <InputGroup size="large" {...formItemLayout}>
-            <Course updateCourses={this.updateWantCourses.bind(this)} passedSelected={this.state.dontWantCourses} sign="1" placeholder="Courses You want" />
-            <Course updateCourses={this.updateDontWantCourses.bind(this)} passedSelected={this.state.wantCourses} sign="0" placeholder="Courses You don't want" />
+            <Course updateCourses={this.updateWantCourses.bind(this)}
+              passedSelected={this.state.dontWantCourses} sign={true}
+              placeholder="Courses You want" />
+            <Course updateCourses={this.updateDontWantCourses.bind(this)}
+              passedSelected={this.state.wantCourses} sign={false}
+              placeholder="Courses You don't want" />
           </InputGroup>
           <br />
           <InputGroup size="large" {...formItemLayout}>
-            <Holiday updateHoliday={this.updateWantOnHolidays.bind(this)} sign="1" passedSelected={this.state.dontWantOnHolidays} placeholder="Holidays/Specific You want" />
-            <Holiday updateHoliday={this.updateDontWantOnHolidays.bind(this)} sign="0" passedSelected={this.state.wantOnHolidays} placeholder="Holidays/Specific You don't want" />
+            <Holiday updateHoliday={this.updateWantOnHolidays.bind(this)}
+              sign={true} passedSelected={this.state.dontWantOnHolidays}
+              placeholder="Holidays/Specific You want" />
+            <Holiday updateHoliday={this.updateDontWantOnHolidays.bind(this)}
+              sign={false} passedSelected={this.state.wantOnHolidays} 
+              placeholder="Holidays/Specific You don't want" />
           </InputGroup>
           <br />
-          <input type="submit" />
+
           <Button type="primary" htmlType="submit" icon="search">
             Search Recipes
           </Button>
@@ -166,7 +183,8 @@ class SearchForm extends Component {
           <label>Holidays you want: {this.state.wantOnHolidays}</label>
           <br />
           <label>Holidays you don't want: {this.state.dontWantOnHolidays}</label>
-          <br /><br />
+          <br />
+          <br />
         </div>
       </div>
     );
