5eadd805d1c535a7e045051817bebaa211fa8a7f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-07-10 02:40:54 +0300

    +

--- src/components/SearchForm/SelectContainer/UnitedSelectField.js
+++ src/components/SearchForm/SelectContainer/UnitedSelectField.js
@@ -54,7 +54,16 @@ class UnitedSelectField extends Component {
       );
     }
 
+    const components = {
+      photo: PhotoStory,
+      video: VideoStory
+    };
 
+    function Story(props) {
+      // Correct! JSX type can be a capitalized variable.
+      const SpecificStory = components[props.storyType];
+      return <SpecificStory story={props.story} />;
+    }
   }
 
   renderChildrens(){
